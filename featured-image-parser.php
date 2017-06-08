cda154589e9cf728c55f3c54bd1c7c55b0ee8c7d
Jonathan Cox <cbjcaesar@gmail.com>
2017-06-08 12:02:24 -0400

    Added link checking, redirect handling, and initial parsing of html

--- featured-image-parser.php
+++ featured-image-parser.php
@@ -10,6 +10,23 @@ License: MIT
 */
 
 /* gets the data from a URL */
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+function test_url($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    curl_exec($ch); // $a will contain all headers
+
+    $url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+    return $url;
+}
+
 function get_data($url) {
     $ch = curl_init();
     $timeout = 5;
@@ -26,17 +43,23 @@ function get_post_id () {
     global $post;
     $id = $post->ID;
 
-    if( is_single($id) && get_post_format($id) == 'link' ) {
-        $url = 'http://thedevknight.com/';
-        $html = get_data($url);
+    $page_object = get_post( $id );
+    $content = $page_object->post_content;
 
-        preg_match_all( '|<img.*?src=[\'"](.*?)[\'"].*?>|i',$html, $matches );
+    @$content = preg_match_all( '/href\s*=\s*[\"\']([^\"\']+)/', $content, $links );
+    $content = $links[1][0];
 
-        echo "heres the count: ".count($matches);
+    if( !empty($content) && is_single($id) && get_post_format($id) == 'link' ) {
+        $url = test_url( strval($content) );
 
-        for($i = 0;$i<count($matches[1]);$i++){
-            $match = $matches[ 1 ][ $i ];
-            echo "<img src=\"$url$match\"></img>";
+        $dom = new DOMDocument;
+        @$dom->loadHTML(get_data($url));
+        $html = $dom->saveHTML();
+        $xpath = new DOMXPath($dom);
+        $nlist = $xpath->query("//meta/@content");
+        foreach($nlist as $n){
+            echo $n->nodeValue;
+
         }
     }
 
